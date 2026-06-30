dae83e5fa103f22467b8c5ade0fa532202c538f0
Marius Wassink <Marius.Wassink@inomatic.de>
2026-06-30 11:44:51 +0200

    *

--- src/main.c
+++ src/main.c
@@ -13,18 +13,18 @@ int main(void)
     //volatile uint32_t afl8_idr = *(volatile uint32_t *)0x000A81A0;
     //volatile uint32_t afcr = *(volatile uint32_t *)0x000A8028;
     
-    if((CANFD.AFCR.BIT.PAGE) == 0)
-    {
-      //if(afl8_idr == 0x00000020)
-      if(CANFD.AFL[8].IDR.BIT.ID == 0x00000020)
-      {
-        AUSGANG_1 = 1;
-      }
-      else
-      {
-        AUSGANG_1 = 0;
-      }
-    }
-    function_CAN();
+//    if((CANFD.AFCR.BIT.PAGE) == 0)
+//    {
+//      //if(afl8_idr == 0x00000020)
+//      if(CANFD.AFL[8].IDR.BIT.ID == 0x00000020)
+//      {
+//        AUSGANG_1 = 1;
+//      }
+//      else
+//      {
+//        AUSGANG_1 = 0;
+//      }
+//    }
+//    function_CAN();
   }
 }
